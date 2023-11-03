826dcdc2ea7d22e06395e9e0d13b3609012d99d1
DavidODW <supplice@gmail.com>
2023-11-03 13:54:45 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -24,42 +24,34 @@ let i=0;
 dot_slide_function (array_selected);
 /*function gestion des fleches */
 function select_slide_image (array_lenght,array_selected) {
-	slider_arrow_left.addEventListener('mousedown', function(event) {
-		if (event.button === 0) {
-			array_selected--;
-			if (array_selected<0){
-				array_selected=array_lenght- 1;
-			}
-			dot_erase_function();
-			dot_slide_function(array_selected);
-			console.log('arro_left Clic gauche'+array_selected);
-		} else if (event.button === 2) {
-	
-			console.log('arrow_left Clic droit');
+	slider_arrow_left.addEventListener('click', function() {
+		array_selected--;
+		if (array_selected<0){
+			array_selected=array_lenght- 1;
 		}
-	})
-	
-	slider_arrow_right.addEventListener('mousedown', function(event) {
-		if (event.button === 0) {
-			array_selected++;
-			if (array_selected>=array_lenght){
-				array_selected=0;
-			}
-			dot_erase_function();
-			dot_slide_function(array_selected);
-			console.log('arrow_right Clic gauche'+array_selected);
+		dot_erase_function();
+		image_slide_function(array_selected);
+		dot_slide_function(array_selected);
+		console.log('arro_left Clic gauche'+array_selected);
 
-		} else if (event.button === 2) {
+	});
 	
-			console.log('arrow_right Clic droit');
+	slider_arrow_right.addEventListener('click', function(event) {
+		array_selected++;
+		if (array_selected>=array_lenght){
+			array_selected=0;
 		}
+		dot_erase_function();
+		image_slide_function(array_selected);
+		dot_slide_function(array_selected);
+		console.log('arrow_right Clic gauche'+array_selected);
 	});
 	
 	return array_selected;
 }
 
 
-
+select_slide_image(array_lenght,array_selected);
 
 console.log(array_selected);
 /*function génération des points */
@@ -85,3 +77,8 @@ function dot_erase_function() {
 	}
 }
 
+/* function pour changer l'image de la banniére*/
+function image_slide_function (array_selected){
+	let SliderImage = document.getElementById('slider_img');
+	SliderImage.src='./assets/images/slideshow/'+slides[array_selected].image
+}
